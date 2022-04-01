c1c477cb2d023e42c86ff321e184d75c4af135e6
abhishekY2401 <abhiyd2401@gmail.com>
2022-04-01 22:37:45 +0530

    rendered login and home function

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Login from "./Login";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>BillBookSystem</h1>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
